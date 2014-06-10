55c8a2f4924d858b49cca238b556276ef4ee8843
Splintex <eugene.godin@gmail.com>
2014-06-10 16:21:14 +0300

    intro with popup

--- js/common.js
+++ js/common.js
@@ -3,18 +3,16 @@ head.ready(function() {
 	var agent = navigator.userAgent,
 	event = (agent.match(/iPad/i)) ? "touchstart" : "click";
 
-	$(document).bind(event, function(e){
-		$(".js-popup").hide();
-	});
+
 
 	$(document).bind(event, function(e){
 		$(".js-overlay").hide();
-		$("body").removeClass("has-open-popup");
-		$(".js-select-list").hide();
-		$(".js-select").removeClass("is-active");
-		$(".js-extend-item").removeClass("is-active");
-		$(".js-item-popup").fadeOut(200);
-		$(".js-window").fadeOut(200);
+		$("html").removeClass("has-open-popup");
+		//$(".js-select-list").hide();
+		//$(".js-select").removeClass("is-active");
+		//$(".js-extend-item").removeClass("is-active");
+		//$(".js-item-popup").fadeOut(200);
+		//$(".js-window").fadeOut(200);
 	});
 
 	$(".js-extend-item-link").on("click", function(event){
@@ -23,7 +21,7 @@ head.ready(function() {
 		return false;
 	});
 
-	$('.js-intro-slider').slick({
+	$('.js-slider').slick({
 		slidesToShow: 1,
 		infinite: true,
 		speed: 300,
@@ -33,8 +31,37 @@ head.ready(function() {
 		autoplay: true,
   		autoplaySpeed: 5000,
 		onInit: function(){
-			//$(".slider-banner").addClass("is-ready");
+			//alert();
+			//$(".js-slider").addClass("is-ready");
 		}
+	});
+
+	// popups
+	$(".js-popup-link").on("click", function(event){
+		$(".js-overlay").fadeOut(200);
+		var popup = $(this).attr("href");
+		$("html").addClass("has-open-popup");
+		$("."+popup).parent().fadeIn(200);
+		$("."+popup).find('.js-slider-popup').slick({
+			slidesToShow: 1,
+			infinite: true,
+			speed: 300,
+			touchMove: true,
+			dots: true,
+			autoplay: true,
+	  		autoplaySpeed: 5000,
+		});
+		event.stopPropagation();
+		return false; 
+	});
+
+	$(".js-popup-close").on("click", function(){
+		$(".js-overlay").fadeOut(200); 
+		$("html").removeClass("has-open-popup")
+		return false;
+	});
+	$(".js-popup").on("click", function(event){
+		event.stopPropagation();
 	});
 
 });
