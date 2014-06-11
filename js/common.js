14dde84a126c4c7df7f4edf74e58b463178a8655
Splintex <eugene.godin@gmail.com>
2014-06-11 17:01:37 +0300

    user acc without sidebar

--- js/common.js
+++ js/common.js
@@ -10,13 +10,13 @@ head.ready(function() {
 		$("html").removeClass("has-open-popup");
 		//$(".js-select-list").hide();
 		//$(".js-select").removeClass("is-active");
-		//$(".js-extend-item").removeClass("is-active");
+		$(".js-drop").removeClass("is-active");
 		//$(".js-item-popup").fadeOut(200);
 		//$(".js-window").fadeOut(200);
 	});
 
-	$(".js-extend-item-link").on("click", function(event){
-		$(this).parents(".js-extend-item").toggleClass("is-active");
+	$(".js-drop-link").on("click", function(event){
+		$(this).parents(".js-drop").toggleClass("is-active");
 		event.stopPropagation();
 		return false;
 	});
@@ -64,4 +64,137 @@ head.ready(function() {
 		event.stopPropagation();
 	});
 
+	//rating
+	if ($('.js-rating').length > 0) {
+		$('.js-rating').each(function(){
+			$(this).raty({
+				//width: 120,
+				hints: ['Отвратительно', 'Плохо', 'Неплохо', 'Очень хорошо', 'Отлично'],
+				starOff: 'img/star.png',
+				starOn : 'img/star-act.png',
+				score: function() {
+					return $(this).attr('data-score');
+				}
+			});
+		});
+			
+	}
+	if ($('.js-rating-small').length > 0) {
+		$('.js-rating-small').each(function(){
+			$(this).raty({
+				width: 62,
+				hints: ['Отвратительно', 'Плохо', 'Неплохо', 'Очень хорошо', 'Отлично'],
+				starOff: 'img/star-s.png',
+				starOn : 'img/star-s-act.png',
+				score: function() {
+					return $(this).attr('data-score');
+				}
+			});
+		});
+			
+	}
+
+	if ($('.js-rating-read').length > 0) {
+		$('.js-rating-read').each(function(){
+			$(this).raty({
+				//width: 120,
+				readOnly: true,
+				hints: ['Отвратительно', 'Плохо', 'Неплохо', 'Очень хорошо', 'Отлично'],
+				starOff: 'img/star.png',
+				starOn : 'img/star-act.png',
+				score: function() {
+					return $(this).attr('data-score');
+				}
+			});
+		});
+	}
+	if ($('.js-rating-small-read').length > 0) {
+		$('.js-rating-small-read').each(function(){
+			$(this).raty({
+				width: 62,
+				readOnly: true,
+				hints: ['Отвратительно', 'Плохо', 'Неплохо', 'Очень хорошо', 'Отлично'],
+				starOff: 'img/star-s.png',
+				starOn : 'img/star-s-act.png',
+				score: function() {
+					return $(this).attr('data-score');
+				}
+			});
+		});
+	}
+
+	$(".js-download-btn").on("click",function(){
+		$(this).parents(".js-download").find(".js-download-input").trigger("click")
+	});
+
+	if ($(".js-input-tel").length) {
+        $(".js-input-tel").mask("+99(999)999-99-99");
+    }
+
+    $(".textarea textarea").focusin(function() {
+	  $(this).parent().addClass("is-focused")
+	});
+	$(".textarea textarea").focusout(function() {
+	  $(this).parent().removeClass("is-focused")
+	});
+
+	$(".js-select-multi").multiselect({
+		selectedList: 7,
+		noneSelectedText: $(this).attr("data-placeholder"),
+		header: "",
+ 		open: function () {
+ 			//$(this).multiselect("widget").addClass("is-focused");
+ 		},
+ 		close: function () {
+ 			//$(this).multiselect("widget").find("input[type='search']:first").val("");
+ 			//$(this).multiselect("widget").find(".ui-multiselect-checkboxes li").removeAttr("style");
+ 		}
+	}).on("multiselectclick", function(event, ui) {
+		if ($(this).multiselect("widget").find("input").is(":checked")) {
+			$(this).next().addClass("has-value").removeClass("has-placeholder");
+		}
+		else {
+			$(this).next().addClass("has-placeholder").removeClass("has-value");
+		}
+
+	});;
+	$(".js-select-width").multiselect({
+		selectedList: 7,
+		noneSelectedText: $(this).attr("data-placeholder"),
+		header: "",
+ 		open: function () {
+ 			$(this).multiselect("widget").addClass("is-full-width");
+ 		},
+ 		close: function () {
+ 			$(this).multiselect("widget").removeClass("is-full-width");
+ 		}
+	}).on("multiselectclick", function(event, ui) {
+		if ($(this).multiselect("widget").find("input").is(":checked")) {
+			$(this).next().addClass("has-value").removeClass("has-placeholder");
+		}
+		else {
+			$(this).next().addClass("has-placeholder").removeClass("has-value");
+		}
+
+	});;
+	$(".js-select-age").multiselect({
+		selectedList: 1,
+		noneSelectedText: $(this).attr("data-placeholder"),
+		header: "",
+		multiple: false,
+ 		open: function () {
+ 			$(this).multiselect("widget").addClass("is-small");
+ 		},
+ 		close: function () {
+ 			$(this).multiselect("widget").removeClass("is-small");
+ 		}
+	}).on("multiselectclick", function(event, ui) {
+		if ($(this).multiselect("widget").find("input").is(":checked")) {
+			$(this).next().addClass("has-value").removeClass("has-placeholder");
+		}
+		else {
+			$(this).next().addClass("has-placeholder").removeClass("has-value");
+		}
+
+	});
 });
